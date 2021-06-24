18949e275155360487ceac5b6161aacb9b4ebc88
smkim <dearelfoov@gmail.com>
2021-06-24 16:04:09 +0900

    typeorm 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UserModule]
+  imports: [    
+    
+    UserModule,
+  ]
 })
 export class AppModule {}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity('TBL_USER')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+}
